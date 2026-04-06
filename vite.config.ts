37a3aa8d881e4280d5b2f39c5e4ed5c05e95a54b
NMakarevich <nikoladze529@mail.ru>
2026-04-06 22:50:33 +0300

    config: add aliases

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
       '@app': path.resolve(__dirname, './src/app'),
+      '@shared': path.resolve(__dirname, './src/shared'),
     },
   },
 });
