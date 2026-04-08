fda8232a838ba3ca8cd1c64eec0d47f82c057641
NMakarevich <nikoladze529@mail.ru>
2026-04-08 14:17:35 +0300

    feat: add concat classes util

--- src/shared/ui/Burger/Burger.tsx
+++ src/shared/ui/Burger/Burger.tsx
@@ -1,4 +1,5 @@
 import type { BurgerProps } from '@shared/ui/Burger/types.ts';
+import { concatClasses } from '@shared/utils';
 import { type JSX } from 'react';
 
 import styles from './Burger.module.scss';
@@ -6,9 +7,7 @@ import styles from './Burger.module.scss';
 export const Burger = ({ isOpen, toggleBurger }: BurgerProps): JSX.Element => {
   return (
     <div className={styles.burger} onClick={toggleBurger}>
-      <span
-        className={isOpen ? `${styles.open} ${styles['burger-inner']}` : styles['burger-inner']}
-      ></span>
+      <span className={concatClasses(styles.open, isOpen ? styles['burger-inner'] : '')}></span>
     </div>
   );
 };
