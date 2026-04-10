8acc0323272fbf7e0a9488e594df8e74f717928a
NMakarevich <nikoladze529@mail.ru>
2026-04-10 11:57:35 +0300

    config: add css module to extends

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import("stylelint").Config} */
 export default {
-  extends: ['stylelint-config-standard-scss'],
-  "rules": {
-    "selector-pseudo-class-no-unknown": [
+  extends: ['stylelint-config-standard-scss', 'stylelint-config-css-modules'],
+  rules: {
+    'selector-pseudo-class-no-unknown': [
       true,
       {
-        "ignorePseudoClasses": ["export"]
-      }
-    ]
-  }
+        ignorePseudoClasses: ['export'],
+      },
+    ],
+  },
 };
