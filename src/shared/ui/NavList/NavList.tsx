7fd14fff67bef93071fb742b595b3cc78870caa0
NMakarevich <nikoladze529@mail.ru>
2026-04-27 18:03:39 +0300

    refactor: update navList layout

--- src/shared/ui/NavList/NavList.tsx
+++ src/shared/ui/NavList/NavList.tsx
@@ -7,22 +7,32 @@ export const NavList = memo((props: NavListProps): JSX.Element => {
   const { links, isIconMode, isOpen } = props;
 
   const preparedLinks = useMemo(() => {
-    return links.map((link) => ({ ...link, icon: link.icon ?? link.title[0].toUpperCase() }));
-  }, [links]);
+    return links.map((link) => ({
+      ...link,
+      icon: isIconMode
+        ? (link.icon ?? link.title.substring(0, 1).toUpperCase())
+        : (link.icon ?? null),
+    }));
+  }, [isIconMode, links]);
 
   return (
     <nav className={styles['nav-list']}>
       {preparedLinks.map((link) => (
         <a href={link.href} className={styles['nav-list_item']} title={link.title} key={link.title}>
-          <span className={styles.icon}>{link.icon}</span>{' '}
           {isIconMode ? (
             isOpen ? (
-              <p className={styles.title}>{link.title}</p>
+              <>
+                <span className={styles.icon}>{link.icon}</span>
+                <p className={styles.title}>{link.title}</p>
+              </>
             ) : (
-              ''
+              <span className={styles.icon}>{link.icon}</span>
             )
           ) : (
-            <p className={styles.title}>{link.title}</p>
+            <>
+              {link.icon && <span className={styles.icon}>{link.icon}</span>}
+              <p className={styles.title}>{link.title}</p>
+            </>
           )}
         </a>
       ))}
