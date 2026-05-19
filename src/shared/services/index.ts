e5696c3d5c2e05e59a02f7d125d9b21c64f3e151
NMakarevich <nikoladze529@mail.ru>
2026-05-19 14:08:21 +0300

    feat: add navigation-middleware

--- src/shared/services/index.ts
+++ src/shared/services/index.ts
@@ -1,2 +1,3 @@
 export { fetcher } from './fetcher';
+export { navigationMiddleware } from './navigation-middleware.ts';
 export { type ErrorType } from './types.ts';
---
+++ src/shared/services/navigation-middleware.ts
@@ -0,0 +1,24 @@
+import { routes } from '@app/routes';
+import { redirect } from 'react-router';
+
+const loggedOutAllowedRoutes = [
+  routes.main.path,
+  routes.login.path,
+  routes.registry.path,
+  routes.auth.path,
+];
+
+const loggedInDeniedRoutes = [routes.login.path, routes.registry.path, routes.auth.path];
+
+export async function navigationMiddleware({ request }: { request: Request }) {
+  const isAuth = !!localStorage.getItem('access_token');
+  const requestUrl = request.url.replace('http://localhost:5173', '');
+
+  if (isAuth && loggedInDeniedRoutes.includes(requestUrl)) {
+    throw redirect(routes.main.path);
+  }
+
+  if (!isAuth && !loggedOutAllowedRoutes.includes(requestUrl)) {
+    throw redirect(routes.login.path);
+  }
+}
