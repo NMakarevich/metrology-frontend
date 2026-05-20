3597d9de58d005ecb4a6606aa913b41fe9b7e78d
NMakarevich <nikoladze529@mail.ru>
2026-05-20 13:44:57 +0300

    feat: add close icon

---
+++ src/shared/ui/Icons/Close.tsx
@@ -0,0 +1,15 @@
+import type { JSX } from 'react';
+
+export const Close = ({ size = 24 }: { size?: number }): JSX.Element => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height={`${size}px`}
+      viewBox="0 -960 960 960"
+      width={`${size}px`}
+      fill="currentColor"
+    >
+      <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
+    </svg>
+  );
+};
