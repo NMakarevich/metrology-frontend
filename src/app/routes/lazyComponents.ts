5fbf72f5f2025e52fbd24355cbe5778766627643
NMakarevich <nikoladze529@mail.ru>
2026-06-05 16:21:56 +0300

    refactor: move lazy components

---
+++ src/app/routes/lazyComponents.ts
@@ -0,0 +1,6 @@
+import { lazy } from 'react';
+
+export const AuthLayout = lazy(() => import('@shared/ui/AuthLayout/AuthLayout.tsx'));
+export const RegistryPage = lazy(() => import('@pages/RegistryPage/ui/RegistryPage.tsx'));
+export const LoginPage = lazy(() => import('@pages/LoginPage/ui/LoginPage.tsx'));
+export const HomePage = lazy(() => import('@pages/HomePage/HomePage.tsx'));
--- src/app/routes/router.tsx
+++ src/app/routes/router.tsx
@@ -1,14 +1,9 @@
 import { routes } from '@app/routes';
+import { AuthLayout, HomePage, LoginPage, RegistryPage } from '@app/routes/lazyComponents.ts';
 import { NotFound } from '@pages/NotFound/NotFound.tsx';
 import { navigationMiddleware, titleMiddleware } from '@shared/services';
 import { Layout } from '@shared/ui/Layout';
-import { lazy } from 'react';
 import { createBrowserRouter, Navigate } from 'react-router';
-
-const AuthLayout = lazy(() => import('@shared/ui/AuthLayout/AuthLayout.tsx'));
-const RegistryPage = lazy(() => import('@pages/RegistryPage/ui/RegistryPage.tsx'));
-const LoginPage = lazy(() => import('@pages/LoginPage/ui/LoginPage.tsx'));
-const HomePage = lazy(() => import('@pages/HomePage/HomePage.tsx'));
 
 export const router = createBrowserRouter([
   {
